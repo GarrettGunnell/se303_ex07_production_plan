90b8b044493d6558b12c8c935f7b6e599ed92caf
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-05-16 22:19:06 -0700

    Test production change

--- test/province_test.rb
+++ test/province_test.rb
@@ -9,12 +9,19 @@ class ProvinceTest < Minitest::Test
   describe Province do
     before { @asia = Province.new(sample_province_data) }
 
-    it "knows its shortfall" do
+    it "calculates shortfall" do
       assert_equal(5, @asia.shortfall)
     end
 
-    it "knows its profit" do
+    it "calculates profit" do
       assert_equal(@asia.profit, 230)
+    end
+
+    it "can change production" do
+      @asia.producers[0].production = 20;
+
+      assert_equal(@asia.shortfall, -6);
+      assert_equal(@asia.profit, 292);
     end
   end
 end
